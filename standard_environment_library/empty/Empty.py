f8dac4c95396662b495ea4233bce191a3f392fe9
Jürgen Hahn <juergen.hahn@ur.de>
2022-03-29 14:52:54 +0200

    Add files via upload

---
+++ standard_environment_library/empty/Empty.py
@@ -0,0 +1,13 @@
+from libPySI import PySI
+from plugins.standard_environment_library.SIEffect import SIEffect
+
+
+class Empty(SIEffect):
+    regiontype = PySI.EffectType.SI_CUSTOM
+    regionname = "__ Empty __"
+    region_display_name = "Empty"
+
+    def __init__(self, shape: PySI.PointVector = PySI.PointVector(), uuid: str = "", kwargs: dict = {}) -> None:
+        super().__init__(shape, uuid, "res/empty.png", Empty.regiontype, Empty.regionname, kwargs)
+        self.qml_path = self.set_QML_path("Empty.qml")
+        # self.color = PySI.Color(128, 128, 128, 255)
